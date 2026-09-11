cff554b58b2a7afa9bd77f1ccbd8bc09d259975d
Ali Kadum Hasaan <ali.kadum@outlook.dk>
2026-09-11 03:48:44 +0200

    Fix calculation carousel asset caching

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ if (calcLab) {
   const startCalcLoop = () => {
     stopCalcLoop();
     if (reducedMotion.matches || !calcLabVisible || calcLabPaused) return;
-    calcTimer = window.setInterval(() => showCalculation(activeCalc + 1), 4800);
+    calcTimer = window.setInterval(() => showCalculation(activeCalc + 1), 3200);
   };
 
   calcTabs.forEach((tab, index) => {
